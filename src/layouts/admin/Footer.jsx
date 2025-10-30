46b796fda3fe90e8681154c99db0edd78290453f
Quyenwu <quyennvph26660@gmail.com>
2025-10-30 12:07:39 +0700

    updateadd

--- src/layouts/admin/Footer.jsx
+++ src/layouts/admin/Footer.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="h-[60px] flex flex-col justify-center px-10 border-t border-slate-300">
-      <p className="text-sm">
+    <footer className="min-h-[60px] flex flex-col justify-center px-4 md:px-5 py-1 border-t border-slate-300 bg-white">
+      <p className="text-sm text-gray-700">
         © 2025 - Cửa hàng thời trang The Autumn - Địa chỉ: Ngõ 120 Yên Lãng, Đống Đa, Hà Nội
       </p>
-      <p className="text-sm">
+      <p className="text-sm text-gray-700">
         Email: cskh@theautumn.vn | Hotline: 1900 363 JQK
       </p>
-
-    </div>
+    </footer>
   );
 }
