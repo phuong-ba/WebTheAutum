6d1646c00fb9b33707b88005a3d92c3e47fb605b
VuMinhTuan1 <vuminhtuan1a@gmail.com>
2025-11-29 21:34:56 +0700

    App

--- src/routes/index.routes.jsx
+++ src/routes/index.routes.jsx
@@ -1,7 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import adminRouters from "./admin.routes";
 import allRoutes from "./user.routes";
+import CustomerDisplay from "../components/CustomerDisplay";
 
-const routers = createBrowserRouter([...allRoutes, ...adminRouters]);
+
+const routers = createBrowserRouter([
+ // Route riêng cho màn hình iPad/Display tại quầy
+ {
+   path: "/display",
+   element: <CustomerDisplay />,
+ },
+ ...allRoutes,
+ ...adminRouters,
+]);
+
 
 export default routers;
+
+
+
