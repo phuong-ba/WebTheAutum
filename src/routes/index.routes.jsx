0124349aaa2da95450e7522821396b41c1093736
PhanTac1122001 <tintert1703@gmail.com>
2025-12-03 00:17:37 +0700

    update code

--- src/routes/index.routes.jsx
+++ src/routes/index.routes.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import adminRouters from "./admin.routes";
 import allRoutes from "./user.routes";
+import CustomerDisplay from "../components/CustomerDisplay";
 
 const routers = createBrowserRouter([
-  // Route riêng cho màn hình iPad/Display tại quầy
+  // Route riêng cho màn hình iPad/Display tại quầy 
+  {
+   path: "/display",
+   element: <CustomerDisplay />,
+ },
   ...allRoutes,
   ...adminRouters,
 ]);
